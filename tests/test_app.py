743b0640128c2dd7160857407d796e62b631dd40
sandeshhh10 <156153555+sandeshhh10@users.noreply.github.com>
2026-09-17 13:12:06 +0545

    Test pipeline failure

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,7 +14,7 @@ def test_home():
 
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert response.json["status"] == "running"
 
 
